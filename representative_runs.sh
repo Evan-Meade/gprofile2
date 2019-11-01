04a5f16f34aa18998be94072921ec888511a2d47
Evan Meade <evan.meade@protonmail.com>
2019-11-01 10:46:44 -0500

    Added bash script to sample over discrete z's

---
+++ representative_runs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# representative_runs.sh (gprofile2)
+# Evan Meade, 2019
+# Research group of Dr. Lindsay King (UTD)
+#
+# This bash script conducts trials over a discrete set of parameters
+#
+#
