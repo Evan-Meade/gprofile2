ef8c4506cb9a6664b67ec49cc58ed8e9f960d185
Evan Meade <evan.meade@protonmail.com>
2020-02-09 18:31:55 -0600

    Removed outdated scripts

--- representative_runs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# representative_runs.sh (gprofile2)
-# Evan Meade, 2019
-# Research group of Dr. Lindsay King (UTD)
-#
-# This bash script conducts trials over a discrete set of parameters
-#
-#
